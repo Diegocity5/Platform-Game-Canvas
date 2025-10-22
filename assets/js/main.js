5898215c2a6715714b52b951bbe9b3240b19b944
Diegocity5 <icotecc@gmail.com>
2025-10-22 15:46:56 -0500

    99: Declare una ultima funcion llamada claim para la clase que se encargara de actualizar el ancho y alto a 0 y la posicion y tambien mutar claimed a true

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,12 @@ class CheckPoint {
         ctx.fillStyle = "#f1be32";
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+    claim(){
+        this.width = 0;
+        this.height = 0;
+        this.position.y = Infinity;
+        this.claimed = true;
+    }
 }
 class Platform {
     constructor(x, y){
