abdd232f7c64913ec03958800c9ee0f64f084f63
Diegocity5 <icotecc@gmail.com>
2025-10-21 20:35:08 -0500

    88,89,90: Agregue cuatro expresiones al nuevo array

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,7 +142,12 @@ const animate = ()=> {
             return;
         }
 
-        const platformDetectionRules = [];
+        const platformDetectionRules = [
+            player.position.x >= platform.position.x - player.width / 2,
+            player.position.x  <= platform.position.x + platform.width - player.width / 3,
+            player.position.y + player.height  >= platform.position.y,
+            player.position.y <= platform.position.y + platform.height
+        ];
     });
 }
 
