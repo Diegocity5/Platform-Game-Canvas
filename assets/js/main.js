86140ebe27599a939ff71e09b89c7a16973b6dbd
Diegocity5 <icotecc@gmail.com>
2025-11-03 18:16:06 -0500

    113: Validando si todas las expresiones de mi array checkpointDetectionRules se cumple usando el metodo every

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,6 +195,9 @@ const animate = ()=> {
             player.position.x - player.width <= checkpoint.position.x - checkpoint.width + player.width * 0.9,
             index === 0 || checkpoints[index - 1].claimed === true
         ];
+        if(checkpointDetectionRules.every(rule => rule)){
+            
+        }
     });
 }
 
