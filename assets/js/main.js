5811d9c1f5ecee1c33179768c5ae2a6e1ffdf0ec
Diegocity5 <icotecc@gmail.com>
2025-11-03 18:22:55 -0500

    114: Invocando el metodo del checkpoint que estamos iterando claim() para ocultar el yellow checkpoint y alterar el estado de que fue confirmado

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,7 +196,7 @@ const animate = ()=> {
             index === 0 || checkpoints[index - 1].claimed === true
         ];
         if(checkpointDetectionRules.every(rule => rule)){
-            
+            checkpoint.claim();
         }
     });
 }
